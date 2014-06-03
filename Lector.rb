bc11180bc9c238a09cac3c1c8a5715bfb3423ab9
jimmy-ga <ji.granabar@gmail.com>
2014-06-03 09:06:17 -0600

    Funcionalidad para evaluar expresiones

--- Lector.rb
+++ Lector.rb
@@ -3,6 +3,7 @@ class Lector
 	@@abc = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 	@@lista_ns = ['1','2','3','4','5','6','7','8','9','0','+','-','/','*']
 	@@lista_var = []
+	@@tabla_simbolos=[]
 	
 	def initialize(lista,str) #constructor que tiene el codigo y lista con variables
 		@lista = lista
@@ -104,8 +105,8 @@ class Lector
 		codigo = @str #@str es el codigo SML que contiene el objeto
 		variable = '' #aqui se almacena la varible temporalmente para despues meterla a la lista
 		largo_str = codigo.length #largo del codigo restante
-		while largo_str > 0 and codigo.index('var') do  #codigo.index determina si existe el string "var", si existe devuelve el indice donde se encuentra, sino devuelve nil
-			$indice = codigo.index('var')				   #el primer while recorre todo el codigo en busca de var
+		while largo_str > 0 and codigo.index('val') do  #codigo.index determina si existe el string "var", si existe devuelve el indice donde se encuentra, sino devuelve nil
+			$indice = codigo.index('val')				   #el primer while recorre todo el codigo en busca de var
 			copia = codigo
 			@@lista_var = @@lista_var + [Lector.concatena_var(copia.slice($indice+4..copia.length),"")] #agregar las variables a una lista
 			num_espacios = 0 #variable para determinar
@@ -142,7 +143,116 @@ class Lector
 			variable = '' #La variable se vuelve a poner vacia
 		end
 		#puts @@lista_var
-		puts @lista
+		#puts @lista
+		ambientes(@lista)
+		#pruebaTipos(convierteLista("5 + 2.8989 / 34 - 99676 + 576567"))
+		#pruebaTipos(convierteLista("34 + r"))
+	end
+	def splitear(valor)
+		salida=[]
+		temp=""
+		contador=0
+		while valor[contador].nil? == false do
+			variable=valor[contador]
+			if variable!="+" and variable!="/" and variable!="*" and variable!="-"
+				temp.concat(variable)
+			else
+				salida=salida+[temp]+[variable]
+				temp=""
+			end	
+			contador+=1		
+		end
+		salida=salida+[temp]
+		return salida
+	end
+
+	def convierteLista(valor)
+		#lista= "5 + 2.8989 / 34 - 99676 + 576567".split
+		lista= valor.delete " " #borra los espacios en blanco
+		#metodo para split manteniendo los caracteres
+		lista=splitear(lista)
+		#puts lista.to_s
+		i=0
+		listares=[]
+		while lista[i].nil? == false do
+			posicion=lista[i]
+			if posicion=="+" or posicion=="-" or posicion=="*" or posicion=="/"
+				listares=listares+[posicion]
+			elsif posicion.include? "." or posicion.include? ","
+				posicion=posicion.to_f
+				listares=listares+[posicion]
+			else 
+				for simbolo in @@tabla_simbolos
+					if simbolo[0]==posicion
+						posicion=simbolo[1]
+					end
+				end			
+				listares=listares+[posicion.to_i]
+			end
+			i+=1
+		end
+		#puts @@tabla_simbolos[0].split(",")[1]
+		#puts listares.to_s
+		return listares
+	end
+	
+	def pruebaTipos(lista)
+		contador=0
+		#puts lista
+		while lista[contador].nil? ==false do
+			if lista[contador]=="*"
+			lista[contador]=lista[contador-1]*lista[contador+1]
+			lista.delete_at(contador-1)
+			lista.delete_at(contador)
+			elsif lista[contador]=="/"
+			lista[contador]=lista[contador-1]/lista[contador+1]
+			lista.delete_at(contador-1)
+			lista.delete_at(contador)
+			end
+			contador+=1
+		end
+		while lista.length>1 do
+			contador=0
+			while lista[contador].nil? ==false do
+				if lista[contador]=="+"
+				lista[contador]=lista[contador-1]+lista[contador+1]
+				lista.delete_at(contador-1)
+				lista.delete_at(contador)
+				end
+				if lista[contador]=="-"
+				lista[contador]=lista[contador-1]-lista[contador+1]
+				lista.delete_at(contador-1)
+				lista.delete_at(contador)
+				end
+				contador+=1
+			end
+		end
+		#puts [lista[0],lista[0].class.to_s]
+		return lista[0]
+	end
+
+	def ambientes(lista)
+		for i in lista
+			actual=i.split
+			if actual[0]=="val"
+			valor=""
+				variable=actual[1]
+				comienza=3
+				while actual[comienza].nil? == false do
+					valor.concat(actual[comienza])
+					comienza=comienza+1
+				end
+				numeros= %w{1 2 3 4 5 6 7 8 9 0}
+				#puts valor[0].class.to_s
+				if  numeros.include?(valor[0])
+					#puts "antes: "+valor.to_s
+					valor=pruebaTipos(convierteLista(valor))
+					#puts "despues: "+valor.to_s
+				end
+				@@tabla_simbolos=@@tabla_simbolos+[[variable,valor]]
+				puts "Variable: "+variable+" Valor: "+valor.to_s
+			end
+		end
 	end
 end
 
@@ -154,6 +264,6 @@ class Separador
 	end
 end
 
-#Es un ejemplo de codigo => "fun x(lista:int list) = var x = 9 var y = 10"
-a = Lector.new(["Estas son la variables"],"fun x(lista:int list) = var r = 5 + 2.8989 /34 -099676 + 576567 var largo_lisp = (((5,6),(987,354)),((76,32),(5654,456))) let if x == 1 end var y = 'Hola mundo' var er = 34 + r var z = True var n = [1,2,3] if x>3")
+#Es un ejemplo de codigo => "fun x(lista:int list) = val x = 9 val y = 10"
+a = Lector.new(["Estas son la valiables"],"fun x(lista:int list) = val r = 5 + 2.8989 /34 - 099676 + 576567 val largo_lisp = (((5,6),(987,354)),((76,32),(5654,456))) let if x == 1 end val y = 'Hola mundo' val er = 34 + r val z = True val n = [1,2,3] if x>3")
 a.busca_variables()
